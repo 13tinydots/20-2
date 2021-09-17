1fc2aa5b0de30378adf1c20350c7541b5b53a10a
Thomas Mccleary <thomasmccleary@Thomass-Mac-mini.local>
2021-09-16 19:42:16 -0500

    timer added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import React, { useEffect, useState } from "react";
+
 function App() {
-  return <main />;
+  // create a timer
+  const [count, setCount] = useState(0);
+
+  // create a function to increment the timer
+  useEffect(
+    () => {
+      const timer = setTimeout(() => {
+        setCount(count + 1);
+      }, 1000);
+
+      // clean up the timer whenever the component is re-rendered get rid of    // the old one it reduces memory leaks
+      return () => {
+        clearTimeout(timer);
+      };
+    },
+    // dependency array
+    []
+  );
+
+  return <main>{count}</main>;
+  {
+    /* TODO: add a form with one input field and a submit button */
+  }
 }
 
 export default App;
